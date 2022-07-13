d2878c2ea145d26c6a08e35d460d3e031fbbae9a
Marek Wojciechowicz <marek.wojciechowicz25@gmail.com>
2022-07-13 09:06:20 +0200

    Hotfix

--- Porównator Baz/Entities/Scalenia/MainDbContext.cs
+++ Porównator Baz/Entities/Scalenia/MainDbContext.cs
@@ -12,10 +12,7 @@ namespace Porównator_Baz.Entities
 {
     public class MainDbContext : DbContext
     {
-        //private const string _connectionStringBase = @"User=sysdba;Password=M@r3kwbg;Database= D:\Bazy\Scalenia\Milejczyce porównanie przed po\MILEJCZYCE 2021.FDB; DataSource=localhost; Port=3051;Dialect=3; Charset=NONE;Role=;Connection lifetime=15;Pooling=true;MinPoolSize=0;MaxPoolSize=50;Packet Size=8192;ServerType=0;";
 
-        //public DbSet<Udzialy> Udzialy { get; set; }
-        //public DbSet<Podmioty> Podmioty { get; set; }
         public MainDbContext(string path) : base(ConnectionString.GetConnectionString(path))
         {
         }
